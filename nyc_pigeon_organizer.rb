2cd0b7df4ed23fb9e7e67025c6ea65eb8b111c28
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 01:03:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = data.each_with_object({}) do |(key,value), final_NDS|
+    value.each do |(inner_key, names)| 
+      names.each do |name|
+        if !final_NDS[name]
+          final_NDS[name] = {}
+        end 
+        if !final_NDS[name][key]
+          final_NDS[name][key] = []
+        end
+        final_NDS[name][key].push(inner_key.to_s)
+      end 
+    end 
 end
+end 
