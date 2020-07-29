8f2ece7ad73d3458f78546d2bf36e00c068bad39
Billy Helm <billyhelm88@gmail.com>
2020-07-29 14:22:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry' 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = data[:gender][:male] + 
+    data[:gender][:female]
+  hash = Hash[pigeons.collect { |birds| [birds, find_data(data, birds)] } ]
+  p hash 
+
 end
+
+def find_data(data, pigeon)
+  colors = data[:color].select{|k,v| v.include?(pigeon)}.keys
+  colors_hash = {:color => colors.collect{|c| c.to_s}}
+  gender = data[:gender].select{|k,v| v.include?(pigeon)}.keys
+  gender_hash = {:gender => gender.collect{|word| word.to_s}}
+  lives_hash ={:lives => data[:lives].select{|k,v| v.include?(pigeon)}.keys}
+  new = colors_hash.merge(gender_hash, lives_hash)
+  new 
+end 
